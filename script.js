fe9fcf254b9551c97a1689a06b48b9b81b52d390
Jiaju Li <mrlijiaju@gmail.com>
2025-02-13 04:10:41 +0000

    good
    good work

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ let cdContainer = document.getElementById('cdContainer');
 let copyrightContainer = document.querySelector('.copyright-container');
 let profileImage = document.getElementById('jiaju_li');
 let jiajuMessageBox = document.getElementById('jiaju.jpg');
-/ Mapping of file paths to display names
+
+// Mapping of file paths to display names
 const musicMapping = {
     'assets/musics/single_tracks/nocturne_2.wav': 'Nocturne in E-flat Major, Op. 9 No. 2',
     'assets/musics/single_tracks/claire_de_lune.wav': 'Debussy: Suite bergamasque. L. 75/3. Clair de lune',
@@ -67,51 +68,6 @@ window.addEventListener('load', function() {
     generateMusicList();
 });
 
-=======
-// Mapping of file paths to display names
-const musicMapping = {
-    'assets/musics/single_tracks/nocturne_2.wav': 'Nocturne in E-flat Major, Op. 9 No. 2',
-    'assets/musics/single_tracks/claire_de_lune.wav': 'Debussy: Suite bergamasque. L. 75/3. Clair de lune',
-    'assets/musics/single_tracks/liebestraume.wav': 'Liszt: Liebesträume in A-flat Major, S. 541/3',
-    'assets/musics/single_tracks/sonata_no_8.wav': 'Beethoven: Piano Sonata No. 8 in C Minor, Op. 13/2',
-    'assets/musics/single_tracks/nocturne_19.wav': 'Chopin: Nocturne No. 19 in E-Minor, Op. 72/1'
-};
-
-// Array of video URLs
-const videoUrls = [
-    'https://www.youtube.com/embed/lpMGpoxq_9U?si=U1XJ5Aszbe4o9i6_',
-    'https://www.youtube.com/embed/ne8GID6OmaI?si=XqvMiKoct3Jv-fbE',
-    'https://www.youtube.com/embed/-kBnCqwSO98?si=9Vd9ML9epOkthQsI'
-];
-
-let currentVideoIndex = 0;
-
-// Function to generate the music list
-function generateMusicList() {
-    for (const [filePath, displayName] of Object.entries(musicMapping)) {
-        const musicLink = document.createElement('a');
-        musicLink.href = filePath;
-        musicLink.className = 'music-link';
-        musicLink.textContent = displayName;
-        musicLink.addEventListener('click', function(event) {
-            event.preventDefault();
-            backgroundAudio.src = filePath;
-            playAudio();
-            typeText(`Now Playing: ${displayName}`);
-            console.log('Playing audio:', filePath);
-        });
-        musicList.appendChild(musicLink);
-    }
-}
-
-// Generate the music list on page load
-window.addEventListener('load', function() {
-    backgroundAudio.src = 'assets/musics/single_tracks/tourner_dans_le_vide.wav';
-    console.log('Default audio set on load:', backgroundAudio.src);
-    generateMusicList();
-});
-
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
 function playAudio() {
     backgroundAudio.play();
     cd.style.animationPlayState = 'running';
@@ -126,7 +82,6 @@ function pauseAudio() {
     console.log('Pausing audio');
 }
 
-<<<<<<< HEAD
 // Function to show the explore button
 function showExploreButton() {
     exploreButton.style.display = 'block';
@@ -140,14 +95,8 @@ function hideExploreButton() {
 legendModeButton.addEventListener('click', function() {
     const legendModeText = 'The Legend is Now Began...';
     backgroundAudio.src = 'assets/musics/single_tracks/legend_mode_theme_song.mp3';
-=======
-legendModeButton.addEventListener('click', function() {
-    const legendModeText = 'The Legend is Now Began...';
-    backgroundAudio.src = 'assets/musics/single_tracks/legend_mode_theme_song.mp3';
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
     playAudio();
     console.log('Legend mode button clicked, playing audio:', backgroundAudio.src);
-<<<<<<< HEAD
     legendModeButton.textContent = 'You Are Now in Legend Mode';
     classicalModeButton.textContent = 'Classical Mode'; // Reset the other button's text
     cd.src = 'assets/images/cd.png'; // Reset CD image
@@ -166,37 +115,13 @@ legendModeButton.addEventListener('click', function() {
     exitLegendModeButton.style.display = 'block';
     exitClassicalModeButton.style.display = 'none'; // Hide classical mode exit button
     copyrightContainer.style.display = 'none'; // Hide copyright container
-=======
-    legendModeButton.textContent = 'You Are Now in Legend Mode';
-    classicalModeButton.textContent = 'Classical Mode'; // Reset the other button's text
-    cd.src = 'assets/images/cd.png'; // Reset CD image
-    body.style.backgroundImage = 'url("assets/images/legend_mode_background.jpg")'; // Change background image
-    body.classList.add('legend-mode-active'); // Add class to body for legend mode
-    videoContainer.style.display = 'block';
-    legendVideo.src = videoUrls[currentVideoIndex];
-    document.querySelector('h1').style.display = 'none';
-    document.querySelectorAll('p').forEach(p => p.style.display = 'none');
-    document.querySelector('img').style.display = 'none';
-    stopButton.textContent = 'Play the Video'; // Change stop button text to control video
-    stopButton.onclick = toggleVideo; // Change stop button event to control video
-    exitLegendModeButton.style.display = 'block';
-    exitClassicalModeButton.style.display = 'none'; // Hide classical mode exit button
-    copyrightContainer.style.display = 'none'; // Hide copyright container
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
-});
+}); 
 
-<<<<<<< HEAD
 classicalModeButton.addEventListener('click', function() {
     const classicalModeText = 'You Are Now Back to the 19th Century';
     backgroundAudio.src = 'assets/musics/full_cd/chopin_nocturnes_cd1_pollini/audio.wav';
-=======
-classicalModeButton.addEventListener('click', function() {
-    const classicalModeText = 'You Are Now Back to the 19th Century';
-    backgroundAudio.src = 'assets/musics/single_tracks/sonata_no_8.wav';
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
     playAudio();
     console.log('Classical mode button clicked, playing audio:', backgroundAudio.src);
-<<<<<<< HEAD
     typeText(classicalModeText);
     classicalModeButton.textContent = 'You Are Now in Classical Mode';
     legendModeButton.textContent = 'Enter the Legend Mode'; // Reset the other button's text
@@ -222,33 +147,8 @@ document.getElementById('stopButton').classList.remove('pulse'); // Remove pulse
     exitClassicalModeButton.style.display = 'block';
     exitLegendModeButton.style.display = 'none'; // Hide legend mode exit button
     copyrightContainer.style.display = 'none'; // Hide copyright container
-=======
-    typeText(classicalModeText);
-    classicalModeButton.textContent = 'You Are Now in Classical Mode';
-    legendModeButton.textContent = 'Enter the Legend Mode'; // Reset the other button's text
-    cd.src = 'assets/images/cd_classical.png'; // Change CD image
-    body.style.backgroundImage = 'url("assets/images/baroque_background_2.jpg")'; // Change background image
-    body.classList.add('classical-mode-active'); // Add class to body for classical mode
-    videoContainer.style.display = 'none'; // Hide video container
-    legendVideo.src = ''; // Clear video source
-    document.querySelector('h1').style.display = 'block';
-    document.querySelectorAll('p').forEach(p => p.style.display = 'block');
-    document.querySelector('img').style.display = 'none';
-    stopButton.textContent = 'Pause'; // Reset stop button text
-    stopButton.onclick = function() {
-        if (backgroundAudio.paused) {
-            playAudio();
-        } else {
-            pauseAudio();
-        }
-    };
-    exitClassicalModeButton.style.display = 'block';
-    exitLegendModeButton.style.display = 'none'; // Hide legend mode exit button
-    copyrightContainer.style.display = 'none'; // Hide copyright container
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
 });
 
-<<<<<<< HEAD
 cd.addEventListener('click', function() {
     if (backgroundAudio.paused) {
         playAudio();
@@ -267,18 +167,6 @@ exploreButton.addEventListener('click', function() {
     }
 });
 
-=======
-exploreButton.addEventListener('click', function() {
-    if (isDropdownOpen) {
-        dropdownContent.style.maxHeight = '0';
-        isDropdownOpen = false;
-    } else {
-        dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px';
-        isDropdownOpen = true;
-    }
-});
-
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
 function typeText(text) {
     typingText.style.display = 'block';
     typingText.value = ''; // Reset text content before starting the typing animation
@@ -405,7 +293,6 @@ document.querySelectorAll('.expandable').forEach(section => {
         }
     });
 });
-<<<<<<< HEAD
 
 
 function toggleVideo() {
@@ -539,103 +426,3 @@ profileImage.addEventListener('mouseover', function() {
 profileImage.addEventListener('mouseout', function() {
     jiajuMessageBox.style.display = 'none';
 });
-
-=======
-
-
-function toggleVideo() {
-    const iframe = videoContainer.querySelector('iframe');
-    let src = iframe.src;
-    if (stopButton.textContent === 'Play') {
-        if (!src.includes('autoplay=1')) {
-            src += src.includes('?') ? '&autoplay=1' : '?autoplay=1';
-        }
-        iframe.src = src;
-        stopButton.textContent = 'Pause';
-    } else {
-        src = src.replace('&autoplay=1', '').replace('?autoplay=1', '');
-        iframe.src = src;
-        stopButton.textContent = 'Play the Video';
-    }
-}
-
-exitLegendModeButton.addEventListener('click', function() {
-    body.classList.remove('legend-mode-active'); // Remove legend mode class
-    videoContainer.style.display = 'none'; // Hide video container
-    legendVideo.src = ''; // Clear video source
-    document.querySelector('h1').style.display = 'block';
-    document.querySelectorAll('p').forEach(p => p.style.display = 'block');
-    document.querySelector('img').style.display = 'block';
-    exitLegendModeButton.style.display = 'none';
-    stopButton.textContent = 'Play'; // Reset stop button text
-    stopButton.onclick = function() {
-        if (backgroundAudio.paused) {
-            playAudio();
-        } else {
-            pauseAudio();
-        }
-    };
-    backgroundAudio.pause(); // Stop background audio
-    backgroundAudio.src = 'assets/musics/single_tracks/tourner_dans_le_vide.wav'; // Reset to default audio
-    typeText('You are now back to the home page'); // Change typing text back
-    legendModeButton.textContent = 'Enter the Legend Mode'; // Reset legend mode button text
-    classicalModeButton.textContent = 'Classical Mode'; // Reset classical mode button text
-    cd.src = 'assets/images/cd.png'; // Reset CD image
-    cd.style.animationPlayState = 'paused'; // Stop CD animation
-    body.style.backgroundImage = 'url("assets/images/baroque_background.jpg")'; // Reset background image
-    body.classList.remove('classical-mode-active'); // Remove classical mode class
-    currentVideoIndex = 0; // Reset video index
-    copyrightContainer.style.display = 'block'; // Show copyright container
-});
-
-exitClassicalModeButton.addEventListener('click', function() {
-    body.classList.remove('classical-mode-active'); // Remove classical mode class
-    videoContainer.style.display = 'none'; // Hide video container
-    legendVideo.src = ''; // Clear video source
-    document.querySelector('h1').style.display = 'block';
-    document.querySelectorAll('p').forEach(p => p.style.display = 'block');
-    document.querySelector('img').style.display = 'block';
-    exitClassicalModeButton.style.display = 'none';
-    stopButton.textContent = 'Play'; // Reset stop button text
-    stopButton.onclick = function() {
-        if (backgroundAudio.paused) {
-            playAudio();
-        } else {
-            pauseAudio();
-        }
-    };
-    backgroundAudio.pause(); // Stop background audio
-    backgroundAudio.src = 'assets/musics/single_tracks/tourner_dans_le_vide.wav'; // Reset to default audio
-    typeText('You Are Now Back to the Home Page'); // Change typing text back
-    legendModeButton.textContent = 'Enter the Legend Mode'; // Reset legend mode button text
-    classicalModeButton.textContent = 'Classical Mode'; // Reset classical mode button text
-    cd.src = 'assets/images/cd.png'; // Reset CD image
-    cd.style.animationPlayState = 'paused'; // Stop CD animation
-    body.style.backgroundImage = 'url("assets/images/baroque_background.jpg")'; // Reset background image
-    currentVideoIndex = 0; // Reset video index
-    copyrightContainer.style.display = 'block'; // Show copyright container
-});
-
-videoBackground.addEventListener('ended', function() {
-    currentVideoIndex++;
-    if (currentVideoIndex < videoUrls.length) {
-        videoBackground.innerHTML = `
-            <iframe id="legendVideo" width="1120" height="630" src="${videoUrls[currentVideoIndex]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        `;
-        stopButton.textContent = 'Play the Video';
-    } else {
-        currentVideoIndex = 0;
-        exitLegendModeButton.click(); // Exit legend mode when all videos are finished
-    }
-});
-
-// Add event listeners for profile image hover
-profileImage.addEventListener('mouseover', function() {
-    jiajuMessageBox.style.display = 'block';
-});
-
-profileImage.addEventListener('mouseout', function() {
-    jiajuMessageBox.style.display = 'none';
-});
-
->>>>>>> a974fa50b7bc2310604309413c615854314e27e3
