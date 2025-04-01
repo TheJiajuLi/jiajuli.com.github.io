5d0a8ebd177f11fa08ec19aec349c3d81c6a55fd
Jiaju Li <contact@dreamingpolar.com>
2025-04-01 18:52:36 +0100

    update for 3.0.1 version

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dreamingpolar.com.github.io/',
+  base: '/jiajuli.com.github.io/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
